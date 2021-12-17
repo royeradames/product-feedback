0b4d73019333c24ff36c76bb82dbe23d1f74afc8
royeradames <royeraadames@gmail.com>
2021-12-17 12:47:20 -0500

    📦comments and replays of productRequests

--- src/db/migrations/20211215210852_initial.ts
+++ src/db/migrations/20211215210852_initial.ts
@@ -19,9 +19,30 @@ export async function up(knex: Knex): Promise<void> {
         table.text('status');
         table.text('description');
       })
+      /* comments and replays of productRequests */
+      .createTable('comments', (table) => {
+        table.increments('commentsId');
+        table
+          .integer('productRequestsId')
+          .unsigned()
+          .references('productRequests.ProductRequestsId')
+          .onDelete('RESTRICT')
+          .onUpdate('CASCADE');
+        table.text('content');
+        table.text('replyingTo');
+        table
+          .integer('userId')
+          .unsigned()
+          .references('users.userId')
+          .onDelete('RESTRICT')
+          .onUpdate('CASCADE');
+      })
   );
 }
 
 export async function down(knex: Knex): Promise<void> {
-  return knex.schema.dropTable('users').dropTable('productRequests');
+  return knex.schema
+    .dropTable('users')
+    .dropTable('productRequests')
+    .dropTable('comments');
 }
