90163bf2eb21c0d326e5361b5e84ac8a1abf577a
royeradames <royeraadames@gmail.com>
2021-12-17 12:46:48 -0500

    📦Track  project suggestions and planned, in-progress, and live featueres

--- src/db/migrations/20211215210852_initial.ts
+++ src/db/migrations/20211215210852_initial.ts
@@ -10,9 +10,18 @@ export async function up(knex: Knex): Promise<void> {
         table.text('name');
         table.text('username');
       })
+      /*  Track of of a project suggestions and planned, in-progress, and live featueres */
+      .createTable('productRequests', (table) => {
+        table.increments('productRequestsId');
+        table.text('title');
+        table.text('category');
+        table.integer('upvotes');
+        table.text('status');
+        table.text('description');
+      })
   );
 }
 
 export async function down(knex: Knex): Promise<void> {
-  return knex.schema.dropTable('users');
+  return knex.schema.dropTable('users').dropTable('productRequests');
 }
