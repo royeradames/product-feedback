d7e17248324f28023b0e2e18817669f05747e9c6
royeradames <royeraadames@gmail.com>
2021-12-17 12:33:00 -0500

    📦define how to handle knex in dev, testing, and production

---
+++ knexfile.ts
@@ -0,0 +1,60 @@
+/*  define how to handle knex in dev, testing, and production */
+// update with your config settings
+const pgConnection = process.env.DATABASE_URL;
+
+export default {
+  development: {
+    client: 'sqlite3',
+    connection: {
+      filename: './src/db/product-feedback.db3',
+    },
+    useNullAsDefault: true,
+    migrations: {
+      directory: './src/db/migrations',
+      tableName: 'knex_migrations',
+    },
+    seeds: {
+      directory: './src/db/seeds',
+    },
+    pool: {
+      afterCreate: (conn: any, done: any) => {
+        conn.run(`PRAGMA foreign_key = ON`, done);
+      },
+    },
+  },
+  testing: {
+    client: 'sqlite3',
+    connection: {
+      filename: ':memory',
+    },
+    useNullAsDefault: true,
+    migrations: {
+      directory: {
+        directory: './src/db/migrations',
+        tableName: 'knex_migrations',
+      },
+    },
+    seeds: {
+      directory: './src/db/seeds',
+    },
+    pool: {
+      afterCreate: (conn: any, done: any) => {
+        conn.run(`PRAGMA foreign_key = ON`, done);
+      },
+    },
+  },
+  production: {
+    client: 'pg',
+    connection: pgConnection,
+    pool: {
+      min: 2,
+      max: 10,
+    },
+    migrations: {
+      directory: './src/db/migrations',
+    },
+    seeds: {
+      directory: './src/db/seeds',
+    },
+  },
+};
