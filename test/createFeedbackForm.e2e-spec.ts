a8224b5c8a3b786c4987bd3d13299f132194a650
royeradames <royeraadames@gmail.com>
2021-12-20 14:09:08 -0500

    🔬create feedback all available types of feedbacks

--- test/createFeedbackForm.e2e-spec.ts
+++ test/createFeedbackForm.e2e-spec.ts
@@ -1,7 +1,9 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
+import * as request from 'supertest';
 import { AppModule } from './../src/app.module';
 import db from '../src/db/dbConfig';
+import { NewFeedback } from '../src/feedback/feedback.service';
 
 describe('AppController (e2e)', () => {
   let app: INestApplication;
@@ -18,5 +20,56 @@ describe('AppController (e2e)', () => {
 
     app = moduleFixture.createNestApplication();
     await app.init();
+  });
+
+  const newFeedbacks: {
+    name: 'feature' | 'enhancement' | 'bug';
+    input: NewFeedback;
+    newFeedbackId: number;
+  }[] = [
+    {
+      name: 'feature',
+      input: {
+        title: 'feature',
+        category: 'feature',
+        description: 'feature',
+      },
+      newFeedbackId: 0,
+    },
+    {
+      name: 'enhancement',
+      input: {
+        title: 'enhancement',
+        category: 'enhancement',
+        description: 'enhancement',
+      },
+      newFeedbackId: 0,
+    },
+    {
+      name: 'bug',
+      input: {
+        title: 'bug',
+        category: 'bug',
+        description: 'bug',
+      },
+      newFeedbackId: 0,
+    },
+  ];
+  newFeedbacks.forEach(async (newFeedback) => {
+    describe(`${newFeedback.name} feedback`, () => {
+      it(`create a new feedback`, async () => {
+        const createdFeedback = await request(app.getHttpServer())
+          .post(`/feedback`)
+          .send(newFeedback.input);
+        // check that the feedback has been created
+        expect(createdFeedback.body).toMatchObject({
+          productRequestsId: expect.any(Number),
+        });
+        expect(createdFeedback.status).toBe(201);
+
+        //store the feedback id
+        newFeedback.newFeedbackId = createdFeedback.body.productRequestsId;
+      });
+    });
   });
 });
