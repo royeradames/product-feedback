468ca4ea80eee4c6a44b6d3ee79f38dc0fb4a68b
royeradames <royeraadames@gmail.com>
2021-12-20 13:17:55 -0500

    📦 join all modules to the app

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-
+import { FeedbackModule } from './feedback/feedback.module';
+import { CommentsModule } from './comments/comments.module';
+import { UsersModule } from './users/users.module';
 @Module({
-  imports: [],
+  imports: [FeedbackModule, UsersModule, CommentsModule],
   controllers: [AppController],
   providers: [AppService],
 })
