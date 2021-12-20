9512f39acc80d70387aba5a948b80af08edcf2b6
royeradames <royeraadames@gmail.com>
2021-12-20 13:15:14 -0500

    📦 return documentation link

--- src/app.service.ts
+++ src/app.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  getDocumentation(): string {
+    return '<h1>Welcome to the feedback-app API</h1> <p><a href="https://documenter.getpostman.com/view/11996006/UVRBkki5" target="_blank">Go to the documentation</a></p>';
   }
 }
