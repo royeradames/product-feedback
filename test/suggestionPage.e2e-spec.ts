a5596d5dcbbefeabca734e90eb4105533ba95f4a
royeradames <royeraadames@gmail.com>
2021-12-20 17:56:53 -0500

    📦init suggestion page tests

---
+++ test/suggestionPage.e2e-spec.ts
@@ -0,0 +1,23 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { INestApplication } from '@nestjs/common';
+import * as request from 'supertest';
+import { AppModule } from '../src/app.module';
+import db from '../src/db/dbConfig';
+
+describe('suggestion page (e2e)', () => {
+  let app: INestApplication;
+  beforeAll(async () => {
+    /* reset db  */
+    await db.migrate.rollback();
+    await db.migrate.latest();
+    await db.seed.run();
+  });
+  beforeEach(async () => {
+    const moduleFixture: TestingModule = await Test.createTestingModule({
+      imports: [AppModule],
+    }).compile();
+
+    app = moduleFixture.createNestApplication();
+    await app.init();
+  });
+});
