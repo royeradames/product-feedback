3db16ab00640ad1a6f2167dc282bc217c802032b
royeradames <royeraadames@gmail.com>
2021-12-20 17:57:21 -0500

    🔬get all suggestion feedback

--- test/suggestionPage.e2e-spec.ts
+++ test/suggestionPage.e2e-spec.ts
@@ -20,4 +20,22 @@ describe('suggestion page (e2e)', () => {
     app = moduleFixture.createNestApplication();
     await app.init();
   });
+  // get all suggestions feedback
+  it('get all suggestion feedback', async () => {
+    const res = await request(app.getHttpServer()).get(
+      `/feedback/status/suggestions`,
+    );
+    expect(res.status).toBe(200);
+    // check that all the feedback the spected fields
+    res.body?.forEach((feedback) => {
+      expect(feedback).toMatchObject({
+        productRequestsId: expect.any(Number),
+        title: expect.any(String),
+        category: expect.any(String),
+        upvotes: expect.any(Number),
+        status: expect.any(String),
+        description: expect.any(String),
+      });
+    });
+  });
 });
