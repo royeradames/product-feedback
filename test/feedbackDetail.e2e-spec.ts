f1aee038462383b0a2ece173ddf2c68942aff8ca
royeradames <royeraadames@gmail.com>
2021-12-20 14:51:41 -0500

    🔬get a feedback by id

--- test/feedbackDetail.e2e-spec.ts
+++ test/feedbackDetail.e2e-spec.ts
@@ -1,7 +1,9 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
+import * as request from 'supertest';
 import { AppModule } from '../src/app.module';
 import db from '../src/db/dbConfig';
+import { Comment, Reply } from '../src/comments/comments.service';
 
 describe('feedback detail page', () => {
   let app: INestApplication;
@@ -18,5 +20,20 @@ describe('feedback detail page', () => {
 
     app = moduleFixture.createNestApplication();
     await app.init();
+  });
+
+  // get a feedback by id
+  it('get a feedback by id', async () => {
+    const res = await request(app.getHttpServer()).get(`/feedback/1`);
+    expect(res.status).toBe(200);
+    // check that all the feedback the spected fields
+    expect(res.body).toMatchObject({
+      productRequestsId: expect.any(Number),
+      title: expect.any(String),
+      category: expect.any(String),
+      upvotes: expect.any(Number),
+      status: expect.any(String),
+      description: expect.any(String),
+    });
   });
 });
