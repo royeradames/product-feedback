7a5a15c482ce2e3d9ae49ed5ffcea1af9eefdc07
royeradames <royeraadames@gmail.com>
2021-12-20 16:19:37 -0500

    📦handle comment creation

--- src/comments/comments.controller.ts
+++ src/comments/comments.controller.ts
@@ -8,4 +8,37 @@ import CommentsValidation from './commentsValidation';
 export class CommentsController {
   constructor(private readonly commentsService: CommentsService) {}
   /* /comments */
+  @Post()
+  async createComment(
+    @Res() res,
+    @Body('productRequestsId') productRequestsId: Comment['productRequestsId'],
+    @Body('content') content: Comment['content'],
+    @Body('userId') userId: Comment['userId'],
+  ) {
+    /* validation */
+    const commentValiation = new CommentsValidation();
+    commentValiation.productRequestsId = productRequestsId;
+    commentValiation.content = content;
+    commentValiation.userId = userId;
+    try {
+      await validateOrReject(commentValiation, {
+        skipMissingProperties: true,
+      });
+    } catch (errors) {
+      res.status(422).json(errors);
+    }
+
+    /* create comment */
+    try {
+      const comment: Comment = {
+        productRequestsId,
+        content,
+        userId,
+      };
+      const newComment = await this.commentsService.createComment(comment);
+      res.status(201).json({ commentsId: newComment });
+    } catch (error) {
+      res.status(404).json(error);
+    }
+  }
 }
